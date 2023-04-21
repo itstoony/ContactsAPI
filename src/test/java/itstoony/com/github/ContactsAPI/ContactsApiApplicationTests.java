87e73fc2beccc1f8b04f98b17558c50859b0b264
Tony <toonyrenner@gmail.com>
2023-04-21 10:25:32 -0300

    Creating Project

---
+++ src/test/java/itstoony/com/github/ContactsAPI/ContactsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package itstoony.com.github.ContactsAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
